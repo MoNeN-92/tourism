ebef5aba8812e52e4f69534b217d081becbe525d
MoNeN-92 <ika.tatishvili@gmail.com>
2026-02-07 21:14:47 +0400

    ready front end to deploy

--- nextjs-frontend/lib/api.ts
+++ nextjs-frontend/lib/api.ts
@@ -1,17 +1,38 @@
-// lib/api.ts
+// // lib/api.ts
+// import axios from 'axios'
+
+// const api = axios.create({
+//   baseURL: 'http://localhost:3001',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+// })
+
+// api.interceptors.request.use((config) => {
+//   const token = localStorage.getItem('token')
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`
+//   }
+//   return config
+// })
+
+// export default api
+
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
 })
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token')
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`
+  if (typeof window !== 'undefined') {
+    const token = localStorage.getItem('token')
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
   }
   return config
 })

62259d1ba0579818bd8b66a8b4602ac34d6e7709
MoNeN-92 <ika.tatishvili@gmail.com>
2026-02-22 22:11:03 +0400

    blog content restore

--- nextjs-frontend/app/[locale]/blog/page.tsx
+++ nextjs-frontend/app/[locale]/blog/page.tsx
@@ -8,7 +8,7 @@ import { useParams } from 'next/navigation'
 import { useTranslations } from 'next-intl'
 import { mockBlogPosts, type BlogPost } from '@/lib/mockBlogData'
 import api from '@/lib/api'
-import { allowMockContent, mergeMockWithApiContent } from '@/lib/content-policy'
+// import { allowMockContent, mergeMockWithApiContent } from '@/lib/content-policy'
 
 interface ApiBlogPost {
   id: string
@@ -94,22 +94,11 @@ export default function BlogPage() {
     fetchApiPosts()
   }, [])
 
-  const shouldMergeMockContent = mergeMockWithApiContent()
-  const fallbackWithMockContent = allowMockContent()
-
   const posts = (() => {
-    if (shouldMergeMockContent) {
-      const apiSlugs = new Set(apiPosts.map(p => p.slug))
-      const filteredMock = mockBlogPosts.filter(p => !apiSlugs.has(p.slug))
-      return [...apiPosts, ...filteredMock]
-    }
-
-    if (apiPosts.length > 0) {
-      return apiPosts
-    }
-
-    return fallbackWithMockContent ? mockBlogPosts : []
-  })()
+  const apiSlugs = new Set(apiPosts.map(p => p.slug))
+  const filteredMock = mockBlogPosts.filter(p => !apiSlugs.has(p.slug))
+  return [...apiPosts, ...filteredMock]
+})()
 
   return (
     <div className="min-h-screen bg-gray-50">
